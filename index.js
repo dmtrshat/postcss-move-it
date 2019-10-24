d5d5afc4e1edb37e75a752e24a7605b57989aac5
Dmitry Shatokhin <dmtrshatokhin@gmail.com>
2019-10-25 01:31:03 +0500

    Add plugin

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// main library file
+const postcss = require("postcss");
+const keyframes = require("./lib/keyframes");
+
+module.exports = postcss.plugin("postcss-animation", () => {
+  let hasKeyframes = [];
+
+  function appendKeyframes(css, value) {
+    if (!keyframes[value] || hasKeyframes[value]) {
+      return;
+    }
+    if (!hasKeyframes[value]) {
+      hasKeyframes[value] = true;
+      css.append(keyframes[value]);
+    }
+  }
+
+  return css => {
+    hasKeyframes = [];
+
+    css.walkDecls("animation-name", decl => {
+      const { value } = decl;
+      appendKeyframes(css, value);
+    });
+
+    css.walkDecls("animation", decl => {
+      const values = postcss.list.space(decl.value);
+      values.forEach(value => {
+        appendKeyframes(css, value);
+      });
+    });
+  };
+});
